5fcfe901905deaead21d908a02dac66b4f9cef24
Shantilal Kopnar <128344061+shantilal-kopnar@users.noreply.github.com>
2024-03-16 00:09:03 +0530

    Add files via upload

---
+++ SwitchExample1.java
@@ -0,0 +1,27 @@
+  import java.util.Scanner;
+  class SwitchExample1
+  {
+
+  	public static void main(String[] args)
+  	{
+  		int a=1;
+  		switch (a)
+  		{
+
+  		case 1: System.out.println("HI FROM CASE 1");
+  		case 2: System.out.println("HI FROM CASE 2");
+  		case 3: System.out.println("HI FROM CASE 3");
+  		case 4: System.out.println("HI FROM CASE 4");
+  		case 5: System.out.println("HI FROM CASE 5");
+  		default :System.out.println("DEFAULT BLOCK");
+
+
+  			
+  		}
+  		}
+  		
+  	}
+
+  
+
+	
