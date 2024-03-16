2382cf2b0d2c583e137611a51720e9732bb46709
Shantilal Kopnar <128344061+shantilal-kopnar@users.noreply.github.com>
2024-03-17 00:26:11 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+  import java.util.Scanner;
+  class VowelSwitch
+  {
+  	public static void main(String[] args) 
+  	{
+  	Scanner sc = new Scanner(System.in);
+  	System.out.println("Enter any Character:");
+  	char ch = sc.nextI().toLowerCase.charAt(0);
+  	Switch(ch)
+  	{
+  	case 'a':
+  	case 'e':
+  	case 'i':
+  	case 'o':
+  	case 'u':System.out.println(ch +" is a Vowel");break;
+  	default:System.out.println(ch +"is a Consonent");break;
+
+  	}
+
+
+
+  	}
+
+  }
+
+
+
+ 
+
+  	
