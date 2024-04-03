14c46cbef3c8a5e50568573273cd3d2316f3bb28
Shantilal Kopnar <128344061+shantilal-kopnar@users.noreply.github.com>
2024-04-03 22:28:43 +0530

    Add files via upload

---
+++ MethodExample1.java
@@ -0,0 +1,22 @@
+class MethodExample1
+{
+	public static void main(String[] args)
+	 {
+		System.out.println("Execution Starts");
+		myMethod();
+		checkEvenOdd(10);
+		System.out.println("Execution Ends");
+	}
+	public static void myMethod()
+	{
+		System.out.println("HELLO FROM myMethod()");
+	}
+	public static void checkEvenOdd(int num)
+	{
+		if(num%2==0){
+			System.out.println(num+ "is even");
+		}else{
+			System.out.println(num+ "is odd");
+		}
+	}
+}
