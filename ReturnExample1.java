b5e1679efc7b6279f5129b768a292cb7450479a7
Shantilal Kopnar <128344061+shantilal-kopnar@users.noreply.github.com>
2024-04-03 22:33:54 +0530

    Add files via upload

---
+++ ReturnExample1.java
@@ -0,0 +1,21 @@
+class ReturnExample1
+{
+	public static void main(String[] args)
+	 {
+		demo('A');
+	}
+	public static void demo(byte a)
+	{
+		System.out.println("byte");
+      
+	}
+	public static void demo(short a)
+	{
+		System.out.println("short");
+
+	}
+	public static void demo(char a)
+	{
+		System.out.println("char");
+	}
+}
