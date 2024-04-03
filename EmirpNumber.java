4eae4ecf681a42e0c4e75a66de0af001c48f54f4
Shantilal Kopnar <128344061+shantilal-kopnar@users.noreply.github.com>
2024-04-03 22:27:53 +0530

    Add files via upload

---
+++ EmirpNumber.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class EmirpNumber
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		if(isPrime(num))
+		{
+			int rev = reverseNumber(num);
+			System.out.println(isPrime(rev)?"Emirp":"Not Emirp");
+		}else{
+			System.out.println("Not Emirp");
+		}
+		
+	}
+	public static boolean isPrime(int num)
+	{
+		if(num<=1){
+			return false;
+		}
+		boolean flag = true;
+		for(int i=2;i<num;i++)
+		{
+			if(num%i==0)
+			{
+				flag=false;
+				break;
+			}
+		}
+		return flag;
+
+	}
+	public static int reverseNumber(int num){
+		int rev=0;
+		while(num>0){
+			rev = rev*10+(num%10);
+			num/=10;
+		}
+		return rev;
+	}
+}
